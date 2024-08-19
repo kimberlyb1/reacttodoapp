0f286b344b659e3fc7d4a09fbbb762a1b2f20e26
Kimberly Bridges <kbridges.prsvr@gmail.com>
2024-08-19 13:53:16 -0500

    added changes

---
+++ redux-todo-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+  <div className="App">
+  <h1>Redux To-Do List</h1>
+  <TodoList />
+  </div>
+  );
+}
---
+++ redux-todo-app/src/actions.js
@@ -0,0 +1,19 @@
+export const addTodo = (text) => ({
+    type: 'ADD_TODO',
+    payload: {text}
+});
+
+export const removeTodo = (id) => ({
+    type: 'REMOVE_TODO',
+    payload: {id}
+});
+
+
+
+export const toggleTodo = (id) => ({
+    type: 'TOGGLE_TODO',
+    payload: {id}
+});
+
+    
+    
